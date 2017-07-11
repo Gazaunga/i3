ce896a1df061f7b664a706c05b996fdd1704898a
Gazaunga <Gazaunga@gmail.com>
2017-07-11 08:55:05 -0400

    Create termite-install.sh

---
+++ termite-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+git clone --recursive https://github.com/thestinger/termite.git
+cd ../termite && make && sudo make install
+cd ~
+git clone https://github.com/thestinger/vte-ng.git
+cd vte-ng && ./autogen.sh && make && sudo make install
+cd ~
+sudo mkdir -p /lib/terminfo/x; sudo ln -s \
+/usr/local/share/terminfo/x/xterm-termite \
+/lib/terminfo/x/xterm-termite
