216892c373ec40cf2ecfd16b32c6944ef12a549d
Gazaunga <Gazaunga@gmail.com>
2017-07-24 13:15:33 -0400

    Update fontstrap.sh

--- .local/bin/.scripts/fontstrap.sh
+++ .local/bin/.scripts/fontstrap.sh
@@ -7,6 +7,11 @@ unzip ultimate_oldschool_pc_font_pack_v1.0.zip
 git clone https://github.com/tonsky/FiraCode.git
 git clone https://github.com/sunaku/tamzen-font.git
 git clone https://github.com/FortAwesome/Font-Awesome.git
+# icons in terminal
+git clone https://github.com/sebastiencs/icons-in-terminal.git
+cd icons-in-terminal
+sh install.sh
+echo "source ~/.local/share/icons-in-terminal/icons_bash.sh" >> $HOME/.bashrc
 # make optional?
 pacaur -S --noconfirm --noedit ttf-hack \
   ttf-ms-fonts \
